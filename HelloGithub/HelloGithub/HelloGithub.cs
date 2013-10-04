d8f53ef7f9ef87e84187355e8125443653c1a34e
niknhattan <nikhattan@yahoo.com>
2013-10-04 18:44:27 +0300

    Renamed Program.cs to HelloGithub.cs

--- HelloGithub/HelloGithub/HelloGithub.cs
+++ HelloGithub/HelloGithub/HelloGithub.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HelloGithub
+﻿namespace HelloGithub
 {
-    class Program
+    using System;
+
+    class HelloGithub
     {
         static void Main()
         {
